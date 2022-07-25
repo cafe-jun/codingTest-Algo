8672d19b004b21284d3107a42d1fec87579dfb60
tb25271@gmail.com <tb25271@gmail.com>
2022-07-25 09:31:01 +0900

    [DualListlist 적용]

--- programmers/2021카카오채용인턴쉽/표편집.py
+++ programmers/2021카카오채용인턴쉽/표편집.py
@@ -1,27 +1,123 @@
-from collections import deque
+from locale import currency
 
 
-
-def solution(n:int, k:int, cmd: list[str]):
-    answer = []
-    node1 = Node(5)
-    node2 = Node(2)
-    node1.next = node2 
-    head = node1 
-    print(node1.data)
-    print(node2.data)
-    # Linked List 를 이용하여 풀기 
-    return answer
-    
-    
 class Node(object):
-    def __init__(self,data,next = None):
+    def __init__(self,data,index):
         self.data = data
-        self.next = next
+        self.prev = None
+        self.next = None
+        self.index = index
     
 class LinkedList(object):
     def __init__(self):
-        pass
+        self.head = Node(None,0)
+        self.head.data = 'O'
+        self.list_size = 1
+    def __str__(self):
+        s = '' 
+        current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+            s += current_node.data
+        return s
+    def insertFirst(self, data,num):
+        new_node = Node(data,num)
+        if not self.head.prev == None:
+            new_node.prev = self.head.prev
+            self.head.prev.next = new_node
+        if not self.head.next == None:
+            new_node.next = self.head.next
+        self.head.prev = new_node
+        self.head = new_node
+      
+    def insertLast(self,data,index):   
+        new_node = Node(data,index)
+        if self.head.next == None:
+            self.head.next = new_node
+            new_node.prev = self.head
+
+        if not self.head.prev == None:
+            self.head.prev.next = new_node
+            new_node.prev = self.head.prev
+        self.head.prev = new_node
+        new_node.next = self.head
+        self.list_size += 1
+        
+    def insertMiddleAfter(self, num, data):
+        node = self.selectNode(num)
+        new_node = Node(data)
+        new_node.prev = node
+        new_node.next = node.next
+        node.next.prev = new_node
+        node.next = new_node
+        self.list_size += 1
+    
+    def selectNode(self, num):
+        if self.list_size < 1:
+            return # Underflow
+        elif self.list_size <= num:
+            return # Overflow
+        count = 0
+        node = self.head
+        if int(self.list_size/2) > num:
+            while count < num:
+                node = node.next
+                count += 1
+        else:
+            repeat = self.list_size - num
+            while count < repeat:
+                node = node.prev
+                count += 1
+        return node
+        
+    
+    def deleteNode(self, num):
+        if self.list_size < 1:
+            return # Underflow
+        elif self.list_size <= num:
+            return # Overflow
+
+        if num == 0:
+            self.deleteHead()
+            return
+        node = self.selectNode(num)
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        # del node 
+        
+
+def solution(n:int, k:int, cmd: list[str]):
+    answer = []
+    # Linked List 를 이용하여 풀기 
+    linked_list = LinkedList()
+    for i in range(1,n):
+        linked_list.insertLast('O',i)
+    print(linked_list)
+#     "U X": 현재 선택된 행에서 X칸 위에 있는 행을 선택합니다.
+    # "D X": 현재 선택된 행에서 X칸 아래에 있는 행을 선택합니다.
+    # "C" : 현재 선택된 행을 삭제한 후, 바로 아래 행을 선택합니다. 단, 삭제된 행이 가장 마지막 행인 경우 바로 윗 행을 선택합니다.
+    # "Z" : 가장 최근에 삭제된 행을 원래대로 복구합니다. 단, 현재 선택된 행은 바뀌지 않습니다.
+    stack_delete = []
+    point = k
+    for c in cmd:
+        if c[0] in ['U','D']:
+            c,move = c.split(' ')
+            if c == 'U':
+                point = max(0,abs(point-int(move)))
+            elif c == 'D':
+                point = min(n-1,abs(point+int(move)))    
+        elif c == 'C':
+            linked_list.deleteNode(point)
+            stack_delete.append(point)
+        elif c == 'Z':
+            delete_point = stack_delete.pop()
+            linked_list.insertIndex(delete_point,'O')
+            
+    print(linked_list)
+    return answer
+    
+    
+  
     
 print(solution(8,2,["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
 # print(solution(8,2,["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
