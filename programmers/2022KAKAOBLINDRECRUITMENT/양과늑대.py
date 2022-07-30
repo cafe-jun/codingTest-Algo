f657e4f23d2033237846dbc34e0a9824ba615937
Cafejun <tb25271@gmail.com>
2022-07-30 23:12:27 +0900

    [Convert]

---
+++ programmers/2022KAKAOBLINDRECRUITMENT/양과늑대.py
@@ -0,0 +1,26 @@
+class Node:
+    result: dict
+
+    def __init__(self, data):
+        self.data = data
+        self.result = {}
+
+
+class Graph:
+    pass
+    # class Tree:
+    #     def __init__(self):
+    #         self.head = Node(0)
+    #         self.count = 0
+
+    #     def insert(self, edges):
+    #         current_node = self.head
+    #         self.count += 1
+    #         while True:
+    #             if edges[0] == current_node:
+
+
+def solution(info, edges):
+    answer = 0
+    for i in enumerate
+    return answer
