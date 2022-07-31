bab0eb5fde7fa2893d66115335b087931f1c34c4
Cafejun <tb25271@gmail.com>
2022-07-31 19:21:35 +0900

    [양과 늑대 commit]

--- programmers/2022KAKAOBLINDRECRUITMENT/양과늑대.py
+++ programmers/2022KAKAOBLINDRECRUITMENT/양과늑대.py
@@ -1,26 +1,36 @@
-class Node:
-    result: dict
+from collections import defaultdict
 
-    def __init__(self, data):
-        self.data = data
-        self.result = {}
-
-
-class Graph:
-    pass
-    # class Tree:
-    #     def __init__(self):
-    #         self.head = Node(0)
-    #         self.count = 0
-
-    #     def insert(self, edges):
-    #         current_node = self.head
-    #         self.count += 1
-    #         while True:
-    #             if edges[0] == current_node:
+answer = -1
 
 
 def solution(info, edges):
-    answer = 0
-    for i in enumerate
+    graph = defaultdict(list)
+    for u, v in edges:
+        graph[u].append(v)
+
+    def dfs(cur, sheep, wolf, next_set):
+        sheep += info[cur] ^ 1
+        wolf += info[cur]
+
+        if sheep <= wolf:
+            return
+        if sheep > wolf:
+            global answer
+            answer = max(answer, sheep)
+            for next_ in next_set:
+                temp = set(graph.get(next_, []))
+                next_set |= temp
+                next_set -= set([next_])
+                dfs(next_, sheep, wolf, next_set)
+                next_set |= set([next_])
+                next_set -= temp
+
+    dfs(0, 0, 0, set(graph.get(0)))
+    print(answer)
     return answer
+
+
+print(solution([0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1], [[0, 1], [1, 2], [1, 4], [
+      0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]) == 5)
+print(solution([0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0], [[0, 1], [0, 2], [
+      1, 3], [1, 4], [2, 5], [2, 6], [3, 7], [4, 8], [6, 9], [9, 10]]) == 5)
