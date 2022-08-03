d2d5948fcf671cf08dd6df9728ddb014a6a65087
Cafejun <tb25271@gmail.com>
2022-08-03 22:37:24 +0900

    [다단계칫솔판매]

---
+++ programmers/2021Dev-Matching웹백엔드개발/다단계칫솔판매.py
@@ -0,0 +1,9 @@
+def solution(enroll, referral, seller, amount):
+    answer = []
+    return answer
+
+
+print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"], ["-", "-", "mary", "edward", "mary", "mary",
+      "jaimie", "edward"], ["young", "john", "tod", "emily", "mary"], [12, 4, 2, 5, 10]) == [360, 958, 108, 0, 450, 18, 180, 1080])
+print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"], ["-", "-", "mary", "edward", "mary", "mary",
+      "jaimie", "edward"], ["young", "john", "tod", "emily", "mary"], [2, 3, 5, 4]) == [0, 110, 378, 180, 270, 450, 0, 0])
