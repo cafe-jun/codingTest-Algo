84892b6eac79246902a1267371a6339759e6d74f
tb25271@gmail.com <tb25271@gmail.com>
2022-09-02 17:31:43 +0900

    [불 왜 시간초과가 나오는 걸끼?]

--- BoJ/불.py
+++ BoJ/불.py
@@ -1,7 +1,5 @@
 
-# input().split()
 from collections import deque
-from copy import copy
 
 answer = 1e9
 f = open('BoJ/test.txt')
@@ -20,39 +18,47 @@ for i in range(r):
             if s == 'F':
                 fires.append((i+1, j+1))
             matrix[i+1][j+1] = s
-f.close()
-print('')
+# f.close()
 q = deque()
-q.append(start)
+tmp_q = deque()
+
 dy = [0, 0, -1, 1]
 dx = [1, -1, 0, 0]
 visited = []
+q.append(start)
 visited.append((start[0], start[1]))
 turn_cost = 0
-# while q:
-#     y, x, cost = q.popleft()
-#     if matrix[y][x] == 'O':
-#         answer = min(cost, answer)
-#         continue
-#     for i in range(4):
-#         ny = y + dy[i]
-#         nx = x + dx[i]
-#         if 0 <= ny <= c+1 and 0 <= nx <= r+1:
-#             if (matrix[ny][nx] == '.' or matrix[ny][nx] == 'O') and (ny, nx) not in visited:
-#                 q.append((ny, nx, cost+1))
-#                 visited.append((ny, nx))
-#     if turn_cost == cost:
-#         tmp_fires = deque()
-#         while fires:
-#             a, b = fires.popleft()
-#             for i in range(4):
-#                 na = a + dy[i]
-#                 nb = b + dx[x]
-#                 if 0 <= na <= c+1 and 0 <= nb <= r+1:
-#                     if matrix[na][nb] == '.':
-#                         matrix[na][nb] = 'F'
-#                         tmp_fires.append((na, nb))
-#         fires = copy(tmp_fires)
+while True:
+    if len(q) == 0:
+        break
+    for _ in range(len(q)):
+        y, x, cost = q.popleft()
+        
+        if matrix[y][x] == 'O':
+            answer = min(cost, answer)
+            continue
+        
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny <= c+1 and 0 <= nx <= r+1:
+                if (matrix[ny][nx] == '.' or matrix[ny][nx] == 'O') and (ny, nx) not in visited:
+                    tmp_q.append((ny, nx, cost+1))
+                    visited.append((ny, nx))
+    q= deque(tmp_q)
+    tmp_q.clear()
+    tmp_fires = deque()
+    while fires:
+        a, b = fires.popleft()
+        for i in range(4):
+            na = a + dy[i]
+            nb = b + dx[i]
+            if 0 <= na <= c+1 and 0 <= nb <= r+1:
+                if matrix[na][nb] == '.' or matrix[na][nb] == 'J':
+                    matrix[na][nb] = 'F'
+                    tmp_fires.append((na, nb))
+    fires = deque(tmp_fires)
+    tmp_fires.clear()
 
 
 if answer == 1e9:
