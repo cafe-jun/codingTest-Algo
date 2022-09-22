54327c22b72d8761c7db0c42b71ca15097c06b39
Cafejun <tb25271@gmail.com>
2022-09-22 22:35:45 +0900

    [다트게임]

--- programmers/2018KAKAOBLINDRECRUITMENT/ 다트게임.py
+++ programmers/2018KAKAOBLINDRECRUITMENT/ 다트게임.py
@@ -1,26 +1,35 @@
-def solution(dartResult: str):
+def solution(dartResult):
     answer = 0
     score = {"S": 1, "D": 2, "T": 3}
     option = {"*": 2, "#": -1}
     tmp_list = []
-    idx = -1
-    for result in dartResult:
-        if result.isnumeric() == True:
-            print('number')
-            tmp_list.append(int(result))
-            idx += 1
+    len_tmp = len(tmp_list)
+    num = ''
+    for idx, result in enumerate(dartResult):
+        if result.isnumeric():
+            num += result
         elif result in score.keys():
-            tmp_list[idx] += tmp_list[idx] ^ score[result]
+            # 숫자 끝 a
+            tmp_list.append(int(num))
+            num = ''
+            tmp_list[-1] = tmp_list[-1] ** score[result]
+            # 다트 연선이 끝인지 확인하기
+            # if idx <= len_tmp:
+            #     # 다음턴
+            #     if result[idx+1].isnumeric():
+            #         continue
+
+            # else:
+            #     continue
         else:
-            tmp_list[idx-1]*option[result]
+            if result == "*":
+                # tmp_list 길이 확인
+                if len(tmp_list) < 2:
+                    tmp_list[0] = tmp_list[0]*option[result]
+                else:
+                    tmp_list[-1] = tmp_list[-1]*option[result]
+                    tmp_list[-2] = tmp_list[-2]*option[result]
+            else:
+                tmp_list[-1] = tmp_list[-1]*option[result]
 
-    return answer
-
-
-print(solution("1S2D*3T") == 37)
-print(solution("1D2S#10S") == 9)
-print(solution("1D2S0T") == 3)
-print(solution("1S*2T*3S") == 23)
-print(solution("1D#2S*3S") == 5)
-print(solution("1T2D3D#") == -4)
-print(solution("1D2S3T*") == 59)
+    return sum(tmp_list)
