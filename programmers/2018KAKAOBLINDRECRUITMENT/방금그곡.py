d1f0e07494d3cfba1977e9e3b5bcfb3107cfd15b
Cafejun <tb25271@gmail.com>
2022-09-24 23:25:46 +0900

    [feat] 방금그곡

--- programmers/2018KAKAOBLINDRECRUITMENT/방금그곡.py
+++ programmers/2018KAKAOBLINDRECRUITMENT/방금그곡.py
@@ -52,3 +52,4 @@ print(solution("CC#BCC#BCC#BCC#B", [
       "03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]) == "FOO")
 print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB",
       "13:00,13:05,WORLD,ABCDEF"]) == "WORLD")
+print('test')
