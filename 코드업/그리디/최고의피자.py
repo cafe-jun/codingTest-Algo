d7a05aa63823c51bd560f1d03718e867d56ab57b
Cafejun <tb25271@gmail.com>
2022-10-11 22:31:50 +0900

    [최고의피자]

---
+++ 코드업/그리디/최고의피자.py
@@ -0,0 +1,13 @@
+# https://codeup.kr/problem.php?id=3321
+
+# N = int(input())
+# A, B = map(int, input().split())
+f = open('./코드업/그리디/test.txt')
+N = int(f.readline())
+A, B = map(int, f.readline().split())
+C = int(f.readline())
+D = []
+for i in range(N):
+    D.append(int(f.readline()))
+
+print('')
