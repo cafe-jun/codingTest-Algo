00fd0f1f7bb68d74901e0f1349cf4877e5d4f2c6
Cafejun <tb25271@gmail.com>
2022-10-12 23:09:32 +0900

    [최고의피자] 10/12일 풀이

--- 코드업/그리디/최고의피자.py
+++ 코드업/그리디/최고의피자.py
@@ -2,7 +2,10 @@
 
 # N = int(input())
 # A, B = map(int, input().split())
+# A + k × B => 한 피자의 가격
+#
 f = open('./코드업/그리디/test.txt')
+
 N = int(f.readline())
 A, B = map(int, f.readline().split())
 C = int(f.readline())
@@ -10,4 +13,5 @@ D = []
 for i in range(N):
     D.append(int(f.readline()))
 
+print('')
 print('')
