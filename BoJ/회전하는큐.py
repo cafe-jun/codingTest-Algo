6bb089f6beea62f6377d1699a929373189f045cf
tb25271@gmail.com <tb25271@gmail.com>
2022-11-13 21:52:55 +0900

    [update] 회전하는큐 문제 이해 안감

---
+++ BoJ/회전하는큐.py
@@ -0,0 +1,11 @@
+# 문제 이해 불가 
+
+
+def solution(m: list[int]):
+    pass
+
+
+print(solution([1,2,3]))
+print(solution([2,9,5]))
+print(solution([27,16,30,11,6,23]))
+print(solution([1,6,3,2,7,9,8,4,10,5]))
