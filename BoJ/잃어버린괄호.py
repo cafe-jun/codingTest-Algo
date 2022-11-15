7ab1759a6ca5a2edb531593de5ea1914f5e902db
Cafejun <tb25271@gmail.com>
2022-11-15 23:38:07 +0900

    [update] 잃어버린 괄호

--- BoJ/잃어버린괄호.py
+++ BoJ/잃어버린괄호.py
@@ -1,5 +1,5 @@
 
-# S = input()
+S = input()
 
 
 def solution(S: str):
@@ -13,11 +13,36 @@ def solution(S: str):
                 arr.append(int(tmp))
                 tmp = ''
             arr.append(char)
-    total = 0
-    switch = '+'
+    else:
+        arr.append(int(tmp))
+
+    total_num = 0
+    split_num = 0
+    switch = False
     for n in arr:
+        if switch == True:
+            if n == '-':
+                total_num += -1*split_num
+                split_num = 0
+            elif n == '+':
+                continue
+            else:
+                split_num += n
+        else:
+            if n == '-':
+                switch = True
+            elif n == '+':
+                continue
+            else:
+                total_num += n
+    else:
+        if switch == True:
+            total_num += -1*split_num
+    return total_num
 
 
-print(solution("55-50+40") == -35)
-print(solution("10+20+30+40") == 100)
-print(solution("00009-00009") == 0)
+print(solution(S))
+
+# print(solution("55-50+40") == -35)
+# print(solution("10+20+30+40") == 100)
+# print(solution("00009-00009") == 0)
