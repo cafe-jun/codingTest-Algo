d5ae5fa0f8cfe97d3a52c996dc474a9680c0a89e
tb25271@gmail.com <tb25271@gmail.com>
2022-11-17 11:12:59 +0900

    [음식평론가] update

---
+++ BoJ/음식평론가.py
@@ -0,0 +1,13 @@
+
+import math
+
+
+
+N,M = map(int,input().split())
+def solution(n,m):
+    return m-math.gcd(n,m)
+    
+print(solution(N,M))
+print(solution(2,6)==4) 
+print(solution(3,4)==3) 
+print(solution(6,2)==0) 
