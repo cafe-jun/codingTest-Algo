4af52ee1be8e06ce96dafd12ffef92c3da7f2b2d
Cafejun <tb25271@gmail.com>
2022-11-19 23:12:29 +0900

    [행운의 문자열]

---
+++ BoJ/행운의문자열.py
@@ -0,0 +1,12 @@
+
+s = input()
+
+
+def solution(s: str):
+    pass
+
+
+print(solution("aabbbaa") == 1)
+print(solution("ab") == 1)
+print(solution("aaab") == 1)
+print(solution("abcdefghij") == 1)
