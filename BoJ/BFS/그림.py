df8df20671da85fa381cb40909e379ccd727473f
tb25271@gmail.com <tb25271@gmail.com>
2022-11-21 22:57:37 +0900

    [그림] 문제풀이

--- BoJ/BFS/그림.py
+++ BoJ/BFS/그림.py
@@ -20,13 +20,19 @@ dy = [0,0,-1,1]
 # 그림 죄표 리스트 
 board = []
 bfs_q = deque()
+# 1인 좌표 돌기 
 while total_q:
     mx,my= total_q.popleft()
+    # 한번 방문한 좌표
     if matrix[mx][my] != 1:
         continue
+    # 한번 방문한 좌표는 0으로 만들기 
     matrix[mx][my] = 0
+    # 0이 아니라면 새로운 그림이라고 판단하고 board 에 넣기 
     board.append([(mx,my)])
+    # bfs_q에 넣기 
     bfs_q.append((mx,my))
+    # bfs 실행 
     while bfs_q:
         x,y = bfs_q.popleft()
         for r in range(4):
@@ -42,7 +48,3 @@ max_cnt = 0
 for b in board:
     max_cnt = max(len(b),max_cnt) 
 print(max_cnt)
-
-
-
-# f.close()
