6e4119839d650b4f7934e76c40fd4234fdd2b6fc
tb25271@gmail.com <tb25271@gmail.com>
2022-11-24 17:28:05 +0900

    [유기농 배추] 유기농 배추

---
+++ BoJ/BFS/유기농배추.py
@@ -0,0 +1,22 @@
+from copy import deepcopy
+
+
+f = open("BoJ/test.txt")
+# T = int(input())
+T = int(f.readline())
+# M 가로 길이 N 은 세로길이 K 배추 좌표 
+# M,N,K = map(int,input().split())
+# M,N,K = map(int,f.readline().split())
+graph_case = []
+point_cases = []
+for _ in range(T):
+    M,N,K = map(int,f.readline().split())
+    point_cases.append([])
+    graph_case.append((M,N))
+    for _ in range(K):
+        point_cases[-1].append(tuple(map(int,f.readline().split())))
+    
+
+print('')
+# copy_g = deepcopy(graph)
+# graph = [[0 for _ in range(N)] for _ in range(M)]
