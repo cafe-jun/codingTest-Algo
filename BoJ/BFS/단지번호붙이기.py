0b2bb3d32ad95aa00533000188614e3dfc1aa82c
tb25271@gmail.com <tb25271@gmail.com>
2022-12-04 00:03:45 +0900

    [단지번호붙이기 완료]

--- BoJ/BFS/단지번호붙이기.py
+++ BoJ/BFS/단지번호붙이기.py
@@ -1,10 +1,15 @@
 
 from collections import deque
+import sys
+input = sys.stdin.readline
+ 
 
 
-f = open('BoJ/test.txt')
-N = int(f.readline())
-graph = [list(map(int,f.readline().strip())) for _ in range(N)]
+# f = open('BoJ/test.txt')
+# N = int(f.readline())
+N = int(input())
+# graph = [list(map(int,f.readline().strip())) for _ in range(N)]
+graph = [list(map(int,input().strip())) for _ in range(N)]
 visited = [[True] * N for _ in range(N)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
@@ -18,7 +23,7 @@ def bfs(point):
         for i in range(4):
             nx,ny = dx[i]+x,dy[i]+y
             if 0<=nx<N and 0<=ny<N:
-                if graph[nx][ny] == 1 and visited[i][j]:
+                if graph[nx][ny] == 1 and visited[nx][ny] == True:
                     q.append((nx,ny))
                     visited[nx][ny] = False
                     result += 1
@@ -29,8 +34,10 @@ def bfs(point):
 answer = []
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 1 and visited[i][j]:
-            visited[i][j] == False
+        if graph[i][j] == 1 and visited[i][j] == True:
+            visited[i][j] = False
             answer.append(bfs((i,j)));
 
-print(answer)
+print(len(answer))
+for a in sorted(answer):
+    print(a)
