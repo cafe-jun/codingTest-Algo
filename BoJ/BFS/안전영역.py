f0f9a10e3f04fbf0cf5fc4e7be1d0face0a1a347
tb25271@gmail.com <tb25271@gmail.com>
2022-12-05 22:57:03 +0900

    [update] 안전영역 잘 모르겠당

--- BoJ/BFS/안전영역.py
+++ BoJ/BFS/안전영역.py
@@ -5,6 +5,38 @@ from collections import deque
 f = open('BoJ/test.txt')
 
 N = int(f.readline())
-region = [list(map(int,f.readline().split())) for _ in range(N)]
-
+region = []
+max_height = 0
+for _ in range(N):
+    temp = list(map(int,f.readline().split()))
+    region.append(temp)
+    for tmp in temp:
+        max_height = max(tmp,max_height)
+        
+                    
+visited = [[True] * N for _ in range(N)]
+q = deque()
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+def bfs(q :deque,t:int):
+    while q:
+        x,y = q.popleft()
+        for k in range(4):
+            nx,ny = dx[k]+x,dy[k]+y
+            if 0<= nx <N and 0 <= ny <N:
+                if visited[nx][ny] and region[nx][ny] <= t:
+                    q.append((i,j))
+                    visited[i][j] = False          
+            
+     
  
+for t in range(1,100):
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] and region[i][j] <= t:
+                q.append((i,j))
+                visited[i][j] = False     
+                bfs(q,t)
+
+            
+    
